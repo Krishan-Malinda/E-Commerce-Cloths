9fdf043354908899335bd2810b1d556da073ffb4
Krishan-Malinda <krishanmalinda99@gmail.com>
2024-05-19 21:32:24 +0530

    Created initial files for the project

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import { routes } from './Routers.jsx';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
