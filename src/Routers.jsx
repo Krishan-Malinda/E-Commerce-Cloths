f32c4405cc5eed11ea4dc2854e1d126091791908
Krishan-Malinda <krishanmalinda99@gmail.com>
2024-05-20 07:56:26 +0530

    Finished AboutUs Page

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,8 +1,18 @@
 import App from "./App";
+import AboutUs from "./Pages/AboutUs";
+import Blog from "./Pages/Blog";
 
 export const routes = [
     {
         path: "/",
-        element: <div>Hello</div>,
+        element: <App />,
+    },
+    {
+        path: "/AboutUs",
+        element: <AboutUs />,
+    },
+    {
+        path: "/Blog",
+        element: <Blog />,
     },
 ];
